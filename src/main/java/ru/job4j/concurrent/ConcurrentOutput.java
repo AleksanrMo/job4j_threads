ca111db98a848f203ff8fdbc35ec4296df723779
AleksandrMo <sushanta@mail.ru>
2022-01-06 22:40:28 +0700

    commit

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+/**
+ * При каждом запуске нити запускаются произвольно т.е вывод идет в произвольном порядке.
+ */
+public class ConcurrentOutput {
+
+    public static void main(String[] args) {
+
+        Thread another = new Thread(
+                () -> System.out.println(
+                        Thread.currentThread().getName()));
+
+        Thread second = new Thread(() -> System.out.println(
+                Thread.currentThread().getName()));
+
+        another.start();
+        second.start();
+    }
+}
