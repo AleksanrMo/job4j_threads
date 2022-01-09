fbe004d6bbbc93c3b7326bca13a16fb191c13329
AleksandrMo <sushanta@mail.ru>
2022-01-09 17:23:44 +0700

    commit

---
+++ src/main/java/ru/job4j/concurrent/Wget2.java
@@ -0,0 +1,45 @@
+package ru.job4j.concurrent;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+
+public class Wget2 implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget2(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                long start = System.currentTimeMillis();
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long rst = System.currentTimeMillis() - start;
+                if (rst < speed) {
+                    Thread.sleep(speed - rst);
+                }
+
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget2(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
