3e0ee5f575550cdd38ef2fa7befacc4ba55abec4
AleksandrMo <sushanta@mail.ru>
2022-01-09 20:39:04 +0700

    commit

--- src/main/java/ru/job4j/concurrent/Wget2.java
+++ src/main/java/ru/job4j/concurrent/Wget2.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
-
 public class Wget2 implements Runnable {
     private final String url;
     private final int speed;
@@ -17,17 +16,19 @@ public class Wget2 implements Runnable {
 
     @Override
     public void run() {
+        String[] s = url.split("/");
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream("t" + s[s.length - 1])) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            long start = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                long start = System.currentTimeMillis();
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 long rst = System.currentTimeMillis() - start;
                 if (rst < speed) {
                     Thread.sleep(speed - rst);
                 }
+                start = System.currentTimeMillis();
 
             }
         } catch (IOException | InterruptedException e) {
@@ -36,6 +37,9 @@ public class Wget2 implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("You must input two arguments, but was " + args.length);
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget2(url, speed));
