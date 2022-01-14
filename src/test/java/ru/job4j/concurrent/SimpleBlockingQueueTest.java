18e621b06f4458374946ae80c777f7d2f4a7180a
AleksandrMo <sushanta@mail.ru>
2022-01-14 21:14:43 +0700

    commit

--- src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -2,6 +2,9 @@ package ru.job4j.concurrent;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 
 public class SimpleBlockingQueueTest {
 
@@ -38,5 +41,38 @@ public class SimpleBlockingQueueTest {
         thread1.join();
         thread2.join();
         Assertions.assertEquals(simple.getSize(), 0);
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(l -> {
+                  try {
+                    queue.offer(l);
+                } catch (InterruptedException ignored) {
+                  }
+
+            } )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (queue.getSize() != 0 || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        Assertions.assertEquals(buffer, Arrays.asList(0, 1, 2, 3, 4));
     }
 }
