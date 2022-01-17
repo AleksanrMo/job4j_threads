63b9cbf03a776f60fafaa44242ae162ab0b64a22
AleksandrMo <sushanta@mail.ru>
2022-01-17 19:07:29 +0700

    commit

--- src/main/java/ru/job4j/concurrent/email/EmailNotification.java
+++ src/main/java/ru/job4j/concurrent/email/EmailNotification.java
@@ -5,22 +5,26 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+   private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         pool.submit(() ->
            send(String.format("Notification %s to email %s.", user.getUserName(), user.getEmail()),
                    String.format("Add a new event to %s.", user.getEmail()), user.getEmail()));
-        close();
     }
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
 
     }
-
-
 }
