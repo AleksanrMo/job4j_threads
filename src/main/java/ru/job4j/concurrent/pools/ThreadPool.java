0f8c995ceebd82f59c6e0be71c132e1393a98cf6
AleksandrMo <sushanta@mail.ru>
2022-01-17 08:31:47 +0700

    commit

--- src/main/java/ru/job4j/concurrent/pools/ThreadPool.java
+++ src/main/java/ru/job4j/concurrent/pools/ThreadPool.java
@@ -1,24 +1,41 @@
 package ru.job4j.concurrent.pools;
 
-import net.jcip.annotations.GuardedBy;
-import org.junit.runner.notification.RunListener;
 import ru.job4j.concurrent.SimpleBlockingQueue;
 
 import java.util.LinkedList;
 import java.util.List;
 
-@RunListener.ThreadSafe
+
 public class ThreadPool {
 
-    @GuardedBy("this")
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(11);
+
+    public ThreadPool() {
+        int size = Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i < size; i++) {
+            threads.add(new Thread(
+                    () -> {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            try {
+                                tasks.poll().run();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+            ));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+    }
 
     public void work(Runnable job) throws InterruptedException {
         tasks.offer(job);
     }
 
-    public synchronized void i() throws InterruptedException {
+    public  void i() throws InterruptedException {
         for (int i = 0; i < tasks.getSize() - 1; i++) {
             threads.add(new Thread(tasks.poll()));
         }
@@ -27,7 +44,7 @@ public class ThreadPool {
         }
     }
 
-    public synchronized void shutdown() throws InterruptedException {
+    public  void shutdown() throws InterruptedException {
         for (Thread t: threads) {
             t.interrupt();
             t.join();
@@ -40,10 +57,6 @@ public class ThreadPool {
         pool.work(() -> System.out.println("2"));
         pool.work(() -> System.out.println("3"));
         pool.work(() -> System.out.println("4"));
-        System.out.println(pool.tasks.getSize());
-        pool.i();
-        Thread.sleep(5000);
         pool.shutdown();
-
     }
 }
